e0b0931fc08d6bbc5a7bf6c016c43069bebed7a9
Justin Joseph <justin.joseph0@yahoo.com>
2016-11-27 16:12:53 -0500

    Style bug list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,6 +8,11 @@
         <div class="container">
             <router-outlet></router-outlet>
         </div>
-    `
+    `,
+    styles: [`
+        .container {
+            margin-top: 5rem;
+        }
+    `]
  })
  export class AppComponent {}
