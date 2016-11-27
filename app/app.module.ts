d5edf8ad567a748eb0005c000720910bd9e6387a
Justin Joseph <justin.joseph0@yahoo.com>
2016-11-26 21:10:48 -0500

    Set up feature and shared modules

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BugsModule } from './bugs/bugs.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [ AppComponent ],
-    imports: [ BrowserModule ],
+    imports: [
+        BrowserModule,
+        BugsModule
+    ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule {}
