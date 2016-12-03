f36b0c35bdab3288be0205bd8371dbd59854d588
Justin Joseph <justin.joseph0@yahoo.com>
2016-12-02 19:54:05 -0500

    Implement sample custom validator

--- app/bugs/bug-details/bug-details.component.ts
+++ app/bugs/bug-details/bug-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { forbiddenStringValidator } from '../../shared/validation/forbidden-string.validator';
+
 @Component({
     moduleId: module.id,
     selector: 'bug-details',
@@ -18,7 +20,7 @@ export class BugDetailsComponent implements OnInit {
 
     configureForm() {
         this._bugForm = new FormGroup({
-            title: new FormControl(null, Validators.required),
+            title: new FormControl(null, [Validators.required, forbiddenStringValidator(/puppy/i)]),
             status: new FormControl(1, Validators.required),
             severity: new FormControl(1, Validators.required),
             description: new FormControl(null, Validators.required)
